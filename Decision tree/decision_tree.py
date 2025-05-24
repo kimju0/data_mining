06478ed2c9c4e73cd6e18ba16f4c8651bc3763b3
kimju0 <kimuniverse0@naver.com>
2025-05-24 19:53:28 +0900

    metrics

--- Decision tree/decision_tree.py
+++ Decision tree/decision_tree.py
@@ -1,25 +1,74 @@
 import sys
+from collections import Counter
+from math import log2
 
 # if len(sys.argv) != 4:
 #     print("Usage: python decision_tree.py <train_file> <test_file> <result_file>")
 #     sys.exit(1)
 TRAIN_FILE_NAME = "dt_train1.txt"  # sys.argv[1]
 TEST_FILE_NAME = "dt_test1.txt"  # sys.argv[2]
-RESULT_FILE_NAME = "dt_answer1.txt"  # sys.argv[3]
+RESULT_FILE_NAME = "dt_answer10.txt"  # sys.argv[3]
 
 
 def read_data(file_name):
     with open(file_name, 'r') as file:
         lines = file.readlines()
     data = [line.strip().split() for line in lines]
-    return data[0], data[1:]
-
-def OneHot_encode(attribute):
+    return data
 
 
-def decision_tree(train_data, test_data):
-    return ["dummy_class" for _ in test_data]
+def write_data(file_name, data):
+    with open(file_name, 'w') as file:
+        for line in data:
+            file.write(' '.join(line) + '\n')
 
 
-attribute, data = read_data(TRAIN_FILE_NAME)
-print(data)
+# format of data: [sample1, sample2, ..., sampleN]
+# format of sample: [attribute1, attribute2, ..., attributeN, label]
+def entropy(data):
+    total_count = len(data)
+    if total_count == 0:
+        return 0
+
+    label_counts = Counter(row[-1] for row in data)
+    entropy_value = 0.0
+
+    for count in label_counts.values():
+        probability = count / total_count
+        entropy_value -= probability * log2(probability)
+
+    return entropy_value
+
+
+def gain_ratio(data, attribute_index):
+    total_entropy = entropy(data)
+    total_count = len(data)
+
+    if total_count == 0:
+        return 0
+
+    attribute_values = {}
+    for row in data:
+        value = row[attribute_index]
+        if value not in attribute_values:
+            attribute_values[value] = []
+        attribute_values[value].append(row)
+
+    weighted_entropy = 0.0
+    split_info = 0.0
+
+    for subset in attribute_values.values():
+        subset_entropy = entropy(subset)
+        probability = len(subset) / total_count
+        weighted_entropy += probability * subset_entropy
+        split_info -= probability * log2(probability)
+
+    if split_info == 0:
+        return 0
+
+    return (total_entropy - weighted_entropy) / split_info
+
+
+train_data = read_data(TRAIN_FILE_NAME)
+attribute, train_data = train_data[0], train_data[1:]
+test_data = read_data(TEST_FILE_NAME)[1:]
