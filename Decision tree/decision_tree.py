da9e59f239289cb630bcb39869ff7fb34b8b338b
kimju0 <kimuniverse0@naver.com>
2025-05-24 21:00:38 +0900

    DecisionTree class

--- Decision tree/decision_tree.py
+++ Decision tree/decision_tree.py
@@ -10,6 +10,62 @@ TEST_FILE_NAME = "dt_test1.txt"  # sys.argv[2]
 RESULT_FILE_NAME = "dt_answer10.txt"  # sys.argv[3]
 
 
+class Node:
+    def __init__(self, attribute=None, label=None, depth=0):
+        self.attribute = attribute
+        self.label = label
+        self.children = {}
+        self.depth = depth
+
+    def is_leaf(self):
+        return self.label is not None
+
+
+class DecisionTree:
+    def __init__(self, data, attributes):
+        self.root = self.build_tree(data, attributes)
+
+    def build_tree(self, data, attributes):
+        if not data:
+            return None
+
+        # Check if all samples have the same label
+        labels = [row[-1] for row in data]
+        if len(set(labels)) == 1:
+            return Node(label=labels[0])
+
+        # If no attributes left, return the most common label
+        if not attributes:
+            most_common_label = Counter(labels).most_common(1)[0][0]
+            return Node(label=most_common_label)
+
+        # Find the best attribute to split on
+        gains = [gain_ratio(data, i) for i in range(len(attributes))]
+        best_attribute_index = gains.index(max(gains))
+
+        # Create a new node for the best attribute
+        node = Node(attribute=best_attribute_index)
+
+        # Split the data based on the best attribute
+        attribute_values = set(row[best_attribute_index] for row in data)
+        for value in attribute_values:
+            subset = [row for row in data if row[best_attribute_index] == value]
+            new_attributes = attributes[:best_attribute_index] + attributes[best_attribute_index + 1:]
+            child_node = self.build_tree(subset, new_attributes)
+            node.children[value] = child_node
+
+        return node
+
+    def classify(self, sample):
+        current_node = self.root
+        while not current_node.is_leaf():
+            attribute_value = sample[current_node.attribute]
+            current_node = current_node.children.get(attribute_value)
+            if current_node is None:
+                return None  # If no child exists for this attribute value
+        return current_node.label
+
+
 def read_data(file_name):
     with open(file_name, 'r') as file:
         lines = file.readlines()
